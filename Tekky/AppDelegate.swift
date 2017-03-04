b38ef355d94c35e6f14486b1a70fba9bf97d618d
Jeremy Rea <jeremyrea@fastmail.com>
2017-03-03 22:42:01 -0500

    Initial Commit

---
+++ Tekky/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Tekky
+//
+//  Created by Jeremy Rea on 2017-03-03.
+//  Copyright © 2017 Jeremy Rea. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
